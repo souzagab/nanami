4b61f0493c9152d1ed70c3c00c91b4951a25c46d
Gabriel S. <me@souzagab.com>
2024-09-30 21:06:05 -0300

    Add initial database configuration

---
+++ app/config/database.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+Base = declarative_base()
