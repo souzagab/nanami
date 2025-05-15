3c21d8f1960d9f01cdef6a9bf0f2551aac81e2ed
Gabriel S. <thesouzagab@gmail.com>
2025-05-15 19:51:34 -0300

    :broom:

--- .old/app/models/base_sql_model.py
+++ .old/app/models/base_sql_model.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel
 
 
 class BaseSQLModel(SQLModel):
-  id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
+  id: int = Field(default=None, primary_key=True, nullable=False)
 
   class Config:
     use_enum_values = True
